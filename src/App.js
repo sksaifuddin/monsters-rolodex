140a665604dc69b4e4d2e9778d061a1d4331fc57
Saif <shaiksaifuddin95@gmail.com>
2022-08-12 19:55:00 +0530

    added search field

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="text"
+          placeholder="Search Monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster, index) => {
+          return <h1 key={index}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
